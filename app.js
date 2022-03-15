5694260cac59c91b87d05543bd184a324ec60b03
Alejandro Nieto <Alejandro.Nieto3@alu.uclm.es>
2022-03-15 16:08:39 +0100

    Added project route to App.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var tasksRouter = require('./routes/tasks');
+var projectsRouter = require('./routes/projects');
 
 
 var app = express();
@@ -28,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/tasks', tasksRouter);
-
+app.use('/projects', projectsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
