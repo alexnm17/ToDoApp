0a222470746383abcdb369f537f648dba2ada750
Diego Sepúlveda Millán <92218221+DiegoSM1998@users.noreply.github.com>
2022-03-15 15:48:27 +0100

    Delete collectionTask.js

--- models/collectionTask.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-const task = require('./task');
-var Schema = mongoose.Schema;
-var collectionTaskSchema = new Schema({
-  collectionName: String,
-  tasks:[{type:Schema.Objetctld, ref:'Task'}]
-});
-module.exports = mongoose.model('', CollectionTaskSchema);colectionTask
